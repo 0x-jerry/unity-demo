fda9e447fa4def10dc1d801917a1f5abc1daa230
Gem Xli <x.li.gem@gmail.com>
2020-08-22 09:47:18 +0800

    init

---
+++ Assets/Robot Kyle/CharacterController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CharacterController : MonoBehaviour
+{
+    [Range(1.0f, 10.0f)]
+    public float moveSpeed = 1.0f;
+
+    Animator animator;
+
+    Transform transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("up"))
+        {
+            animator.SetBool("walk", true);
+
+            var pos = transform.position;
+            transform.position = pos + Vector3.up * moveSpeed;
+        }
+        else if (Input.GetKeyUp("up"))
+        {
+            animator.SetBool("walk", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+}
